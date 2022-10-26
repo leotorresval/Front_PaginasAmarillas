8b442e4d21a22f97063eacb131cd88b88e0f2b1c
leotorresval <leotorresval92@gmail.com>
2022-10-26 16:02:32 -0500

    Diseño inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { FooterComponent } from './footer/footer.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'usuarios',pathMatch:'full'},
+  {path: 'usuarios', component:UsuariosComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UsuariosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
